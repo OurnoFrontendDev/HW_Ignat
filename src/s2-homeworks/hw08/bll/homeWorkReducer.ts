855f16836a01d7936c392a04f6990bb7d9fc7237
OurnoFrontendDev <novikov99951@gmail.com>
2025-03-17 23:52:05 +0300

    first commit5

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,25 @@
-import {UserType} from '../HW8'
+import {initialPeople, UserType} from '../HW8'
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+
+export const homeWorkReducer = (state = initialPeople, action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            const sorted = [...state] // создаём копию
 
-            return state // need to fix
+            if (action.payload === "up") {
+                sorted.sort((a, b) => a.name.localeCompare(b.name))
+            } else {
+                sorted.sort((a, b) => b.name.localeCompare(a.name))
+            }
+
+            return sorted
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(user => user.age >= 18)
         }
         default:
             return state
